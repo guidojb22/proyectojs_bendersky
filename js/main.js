7aaac98972935ce050b9e52fd856fbccadced4b6
Guido <guidobendersky@hotmail.com>
2023-11-20 15:12:32 -0300

    index terminado

--- js/main.js
+++ js/main.js
@@ -125,22 +125,14 @@ llamadoArrayOferta("../js/productos.json", nodosProductosOferta, containerTarjet
 // SWIPER
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
