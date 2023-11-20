3b3f411af5f124ac843317f8f7dd218276e84ede
Guido <guidobendersky@hotmail.com>
2023-11-20 06:59:38 -0300

    oferta y swiper

--- js/main.js
+++ js/main.js
@@ -74,8 +74,7 @@ const llamadoArray = (direccion, generarNodos, container) => {
     fetch(direccion)
     .then(res => res.json())
     .then(data => {
-        generarNodos(data, container)     
-        products = data
+        generarNodos(data, container)  
     })
 }
 
@@ -83,23 +82,66 @@ llamadoArray("../js/productos.json", nodosProductos, containerTarjetas)
 
 // document.body.onclick = ( event ) => {   
 //     if (event.target.classList.contains("productoAgregar")) {    
-//         aniadirAlCarrito(obtenerProducto(event.target.id, products))    
-//         subirAlLS("carrito", carrito)
-//         Toastify({
-//             text: `El producto ${obtenerProducto(event.target.id, products).titulo} ha sido añadido al carrito.`,
-//             duration: 3000,
-//             destination: "https://github.com/apvarun/toastify-js",
-//             newWindow: true,
-//             close: true,
-//             gravity: "top", // `top` or `bottom`
-//             position: "left", // `left`, `center` or `right`
-//             stopOnFocus: true, // Prevents dismissing of toast on hover
-//             style: {
-//               background: "linear-gradient(to right, #00b09b, #96c93d)",
-//             },
-//             onClick: function(){} // Callback after click
-//           }).showToast();       
+//         console.log(products)
 //     }
 // }
 
-// console.log(products)
+// TARJETAS PRODUCTOS OFERTA CON FETCH: JSON
+
+const containerTarjetasOferta = document.querySelector(".oferta");
+
+const nodosProductosOferta = (data, container) => {
+    const nodos = data.reduce((acc, productos) => {
+        return acc + `
+        <article class="producto">
+            <img class="productoImagen" src=${productos.imagen} alt="${productos.titulo}">
+            <div class="productoDetalles">
+                <h3 class="productoTitulo">${productos.titulo}</h3>
+                <p class="productoPrecio">Precio: $${productos.precio}</p>
+                <button class="productoAgregar" id="${productos.id}">Agregar al carrito</button>
+            </div>
+        </article>
+        `;
+    }, "");
+
+    container.innerHTML = nodos;
+};
+
+const llamadoArrayOferta = (direccion, generarNodos, container) => {
+    fetch(direccion)
+        .then((res) => res.json())
+        .then((oferta) => {
+            const productosFiltrados = oferta.filter((objeto) => objeto.oferta === String("si"));
+            console.log(productosFiltrados);
+            generarNodos(productosFiltrados, container);
+        })
+        .catch((error) => {
+            console.error("Error al obtener los datos:", error);
+        });
+};
+
+llamadoArrayOferta("../js/productos.json", nodosProductosOferta, containerTarjetasOferta);
+
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
