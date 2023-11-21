09f59ea3d4fe88b6ed7ca2f010331405d276c58f
Guido <guidobendersky@hotmail.com>
2023-11-21 19:12:38 -0300

    Division en diferentes .js

--- js/main.js
+++ js/main.js
@@ -37,41 +37,6 @@ botonColorMode.addEventListener("click", () => {
     }
 })
 
-// TARJETAS PRODUCTOS OFERTA CON FETCH: JSON
-
-const containerTarjetasOferta = document.querySelector(".oferta");
-
-const nodosProductosOferta = (data, container) => {
-    const nodos = data.reduce((acc, productos) => {
-        return acc + `
-        <article class="producto">
-            <img class="productoImagen" src=${productos.imagen} alt="${productos.titulo}">
-            <div class="productoDetalles">
-                <h3 class="productoTitulo">${productos.titulo}</h3>
-                <p class="productoPrecio">Precio: $${productos.precio}</p>
-                <button class="productoAgregar" id="${productos.id}">Agregar al carrito</button>
-            </div>
-        </article>
-        `;
-    }, "");
-
-    container.innerHTML = nodos;
-};
-
-const llamadoArrayOferta = (direccion, generarNodos, container) => {
-    fetch(direccion)
-        .then((res) => res.json())
-        .then((oferta) => {
-            const productosFiltrados = oferta.filter((objeto) => objeto.oferta === String("si"));
-            console.log(productosFiltrados);
-            generarNodos(productosFiltrados, container);
-        })
-        .catch((error) => {
-            console.error("Error al obtener los datos:", error);
-        });
-};
-llamadoArrayOferta("../js/productos.json", nodosProductosOferta, containerTarjetasOferta);
-
 // TARJETAS PRODUCTOS CON FETCH: JSON Y ORDEN ALFABEETICO CRECIENTE Y DECRECIENTE
 
 const containerTarjetas = document.querySelector("#productosDOM");
