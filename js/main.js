a9d41fe5c8b298222dad4506e0f2fdd8ab4aafec
Guido <guidobendersky@hotmail.com>
2023-11-21 19:03:10 -0300

    orden az y za

--- js/main.js
+++ js/main.js
@@ -37,68 +37,6 @@ botonColorMode.addEventListener("click", () => {
     }
 })
 
-// TARJETAS PRODUCTOS CON FETCH: JSON
-
-const containerTarjetas = document.querySelector("#productosDOM")
-
-// let carrito = JSON.parse(localStorage.getItem("carrito")) || []
-
-// let products;
-
-// const subirAlLS = ( clave, data ) => localStorage.setItem( clave, JSON.stringify(data))
-// const obtenerDelLs = clave => JSON.parse(localStorage.getItem(clave))
-// const aniadirAlCarrito = ( data ) => carrito.push(data)
-// const obtenerProducto = (data) => {
-//     return data.find( element => element.id === Number(obtenerProducto))
-// }
-
-
-const nodosProductos = (data, container) => {
-    const nodos = data.reduce((acc, productos) => {
-        return acc + `
-        <article class="producto">
-            <img class="productoImagen" src=${productos.imagen} alt="${productos.titulo}">
-            <div class="productoDetalles">
-                <h3 class="productoTitulo">${productos.titulo}</h3>
-                <p class="productoPrecio">Precio: $${productos.precio}</p>
-                <button class="productoAgregar" id= "${productos.id}">Agregar al carrito</button>
-            </div>
-        </article>
-        ` 
-    }, "")
-
-    container.innerHTML = nodos
-}
-
-const llamadoArray = (direccion, generarNodos, container) => {
-    fetch(direccion)
-    .then(res => res.json())
-    .then(data => {
-        productos = data;
-        generarNodos(data, container)  
-    })
-}
-
-llamadoArray("../js/productos.json", nodosProductos, containerTarjetas)
-
-// document.body.onclick = ( event ) => {   
-//     if (event.target.classList.contains("productoAgregar")) {    
-//         console.log(products)
-//     }
-// }
-
-// FILTRO BUSQUEDA
-
-const inputBusqueda = document.querySelector("#input-busqueda");
-
-document.querySelector("#form-busqueda").onsubmit = event => {
-    event.preventDefault();
-    const productosBusqueda = productos.filter(producto =>
-        producto.categoria.toLowerCase().includes(inputBusqueda.value.toLowerCase())
-    );
-    nodosProductos(productosBusqueda, containerTarjetas);
-};
-
 // TARJETAS PRODUCTOS OFERTA CON FETCH: JSON
 
 const containerTarjetasOferta = document.querySelector(".oferta");
@@ -132,21 +70,69 @@ const llamadoArrayOferta = (direccion, generarNodos, container) => {
             console.error("Error al obtener los datos:", error);
         });
 };
-
 llamadoArrayOferta("../js/productos.json", nodosProductosOferta, containerTarjetasOferta);
 
-// SWIPER
+// TARJETAS PRODUCTOS CON FETCH: JSON Y ORDEN ALFABEETICO CRECIENTE Y DECRECIENTE
 
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
+const containerTarjetas = document.querySelector("#productosDOM");
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+let productos = [];
 
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+const nodosProductos = (data, container) => {
+    const nodos = data.reduce((acc, producto) => {
+        return acc + `
+        <article class="producto">
+            <img class="productoImagen" src=${producto.imagen} alt="${producto.titulo}">
+            <div class="productoDetalles">
+                <h3 class="productoTitulo">${producto.titulo}</h3>
+                <p class="productoPrecio">Precio: $${producto.precio}</p>
+                <button class="productoAgregar" id="${producto.id}">Agregar al carrito</button>
+            </div>
+        </article>
+        `;
+    }, "");
+
+    container.innerHTML = nodos;
+};
+
+const llamadoArray = (direccion, generarNodos, container) => {
+    fetch(direccion)
+    .then(res => res.json())
+    .then(data => {
+        productos = data;
+        generarNodos(data, container);
+    })
+    .catch(error => {
+        console.error("Error al obtener los datos:", error);
+    });
+};
+
+llamadoArray("../js/productos.json", nodosProductos, containerTarjetas);
+
+document.querySelector("#az").addEventListener("click", function () {
+    const productosOrdenados = productos.sort((a, b) => a.titulo.localeCompare(b.titulo));
+    nodosProductos(productosOrdenados, containerTarjetas);
 });
+
+document.querySelector("#za").addEventListener("click", function () {
+    const productosOrdenados = productos.sort((a, b) => b.titulo.localeCompare(a.titulo));
+    nodosProductos(productosOrdenados, containerTarjetas);
+});
+
+// document.body.onclick = ( event ) => {   
+//     if (event.target.classList.contains("productoAgregar")) {    
+//         console.log(products)
+//     }
+// }
+
+// FILTRO BUSQUEDA
+
+const inputBusqueda = document.querySelector("#input-busqueda");
+
+document.querySelector("#form-busqueda").onsubmit = event => {
+    event.preventDefault();
+    const productosBusqueda = productos.filter(producto =>
+        producto.categoria.toLowerCase().includes(inputBusqueda.value.toLowerCase())
+    );
+    nodosProductos(productosBusqueda, containerTarjetas);
+};
