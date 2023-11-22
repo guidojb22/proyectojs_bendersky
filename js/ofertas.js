727d0573ab6d2fd810dc8fee66be467bd543c5e9
Guido <guidobendersky@hotmail.com>
2023-11-22 20:10:41 -0300

    Agregar al carrito desde oferta

--- js/ofertas.js
+++ js/ofertas.js
@@ -25,8 +25,27 @@ const llamadoArrayOferta = (direccion, generarNodos, container) => {
         .then((res) => res.json())
         .then((oferta) => {
             const productosFiltrados = oferta.filter((objeto) => objeto.oferta === String("si"));
-            console.log(productosFiltrados);
             generarNodos(productosFiltrados, container);
+            document.body.onclick = (event) => {
+                if (event.target.classList.contains("productoAgregar")) {
+                    aniadirAlCarrito((obtenerProducto(event.target.id, productosFiltrados)))
+                    subirAlLS("carrito", carrito)
+                    Toastify({
+                        text: `${obtenerProducto(event.target.id, productosFiltrados).titulo} ha sido añadido al carrito de compras`,
+                        duration: 2200,
+                        destination: "https://github.com/apvarun/toastify-js",
+                        newWindow: true,
+                        close: true,
+                        gravity: "bottom",
+                        position: "right",
+                        stopOnFocus: true,
+                        style: {
+                          background: "linear-gradient(to right, #f42b03ff, #ec7505ff)",
+                        },
+                        onClick: function(){}
+                      }).showToast();  
+                }
+            }
         })
         .catch((error) => {
             console.error("Error al obtener los datos:", error);
